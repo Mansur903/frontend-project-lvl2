5e9a34ab3d7f3a8c827639a050dc75bb76530647
Mansur <mansur98@yandex.ru>
2021-02-22 18:15:46 +0300

    realised flatFileComparsion function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import { flatFileComparsion } from '../files/flatFileComparsion.js';
+
 program.version('0.0.1')
        .description('Compares two configuration files and shows a difference.')
        .arguments('<filepath1> <filepath2>')
+       .action((filepath1, filepath2) => flatFileComparsion(filepath1, filepath2))
        .option('-f, --format [type]', 'output format')
 program.parse(process.argv);
