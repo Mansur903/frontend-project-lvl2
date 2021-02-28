725784fdf204e9396c7bc60322c42f6987f0cae6
Mansur <mansur98@yandex.ru>
2021-02-28 14:26:44 +0300

    added deep json's and yaml's

--- __fixtures__/comparsionResults.js
+++ __fixtures__/comparsionResults.js
@@ -1,4 +1,4 @@
-const file12ComparsionResult = `{
+export const file12ComparsionResultJSON = `{
  - follow: false
    host: hexlet.io
  - proxy: 123.234.53.22
@@ -7,4 +7,14 @@ const file12ComparsionResult = `{
  + verbose: true
 }`;
 
-export default file12ComparsionResult;
+const file12ComparsionResultYAML = `{
+ - follow: false
+ - host: hexlet.io
+ + host: google.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 40
+ + verbose: true
+}`;
+
+export default file12ComparsionResultYAML;
--- __tests__/flatFileComparsion.test.js
+++ __tests__/flatFileComparsion.test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import flatFileComparsion from '../files/flatFileComparsion.js';
-import file12ComparsionResult from '../__fixtures__/comparsionResults.js';
+import file12ComparsionResultYAML, { file12ComparsionResultJSON } from '../__fixtures__/comparsionResults.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,8 +11,8 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 test('flatFileComparsion', () => {
   // eslint-disable-next-line no-undef
   expect(flatFileComparsion(getFixturePath('file1.json'), getFixturePath('file2.json')))
-    .toEqual(file12ComparsionResult);
+    .toEqual(file12ComparsionResultJSON);
   // eslint-disable-next-line no-undef
   expect(flatFileComparsion(getFixturePath('file1.yaml'), getFixturePath('file2.yaml')))
-    .toEqual(file12ComparsionResult);
+    .toEqual(file12ComparsionResultYAML);
 });
