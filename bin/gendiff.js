7ae5bbe7f143762f4022af7dc0552b3070e18591
Mansur903 <mansur98@yandex.ru>
2021-04-13 12:44:46 +0300

    editing gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,11 +3,11 @@ import { program } from 'commander';
 import genDiff from '../files/genTree.js';
 
 program.version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format (default: "stylish")')
   .action((filepath1, filepath2) => {
     const options = program.opts();
     console.log(genDiff(filepath1, filepath2, options.format));
   })
+  .description('Compares two configuration files and shows a difference.')
   .parse(process.argv);
