66613b60a3d4c0fe985a1240158734438c4023f4
Mansur903 <mansur98@yandex.ru>
2021-04-13 12:20:09 +0300

    editing gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program.version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format (default: "stylish")')
   .action((filepath1, filepath2) => {
-    const options = program.opts();
-    console.log(genDiff(filepath1, filepath2, options.format));
+    /* const options = program.opts(); */
+    console.log(genDiff(filepath1, filepath2/* , options.format */));
   })
   .parse(process.argv);
