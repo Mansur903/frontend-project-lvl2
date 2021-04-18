1460ea901dc0cf3948709abd1f31171db297d9ca
Mansur903 <mansur98@yandex.ru>
2021-04-18 17:01:29 +0300

    corrections 1.2

--- __tests__/filesComparsion.test.js
+++ __tests__/filesComparsion.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import genDiff from '../src/files/genTree.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
