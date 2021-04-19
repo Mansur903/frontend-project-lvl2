fffe4b1d56cc509aa01a1736d5856a64053b888e
Mansur903 <mansur98@yandex.ru>
2021-04-19 18:41:21 +0300

    correction 1.295

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,8 +1,3 @@
-/* function isStringValue(item) {
-  if (item === true || item === false || item === null ||) return false;
-  return true;
-} */
-
 function isStringValue(item) {
   if (typeof item !== 'string') return false;
   return true;
@@ -18,43 +13,60 @@ function genPath(path, item) {
   return pathChain;
 }
 
+function generateResult(type, path, firstValue, secondValue) {
+  if (type === 'added') {
+    return `Property '${path}' was added with value: ${firstValue}`;
+  }
+  if (type === 'removed') {
+    return `Property '${path}' was removed`;
+  }
+  if (type === 'changed') {
+    return `Property '${path}' was updated. From ${firstValue} to ${secondValue}`;
+  }
+  return null;
+}
+
 export default function plain(data) {
   function wrapper(thisData, path = []) {
     const thisPath = path;
     const onlyModifiedValues = thisData.filter((item) => item.status !== 'unchanged');
     const result = onlyModifiedValues.map((item) => {
       const generatedPath = genPath(thisPath, item.name);
+      const resultPath = generatedPath.join('.');
       if (item.status === 'added') {
-        if (!isStringValue(item.value)) {
-          return `Property '${generatedPath.join('.')}' was added with value: ${item.value}`;
+        if (isItemObject(item.value)) {
+          return generateResult('added', resultPath, '[complex value]');
         }
-        if (typeof item.value === 'object') {
-          return `Property '${generatedPath.join('.')}' was added with value: [complex value]`;
-        } return `Property '${generatedPath.join('.')}' was added with value: '${item.value}'`;
+        if (!isStringValue(item.value)) {
+          return generateResult('added', resultPath, item.value);
+        }
+        return generateResult('added', resultPath, `'${item.value}'`);
       }
       if (item.status === 'removed') {
-        return `Property '${generatedPath.join('.')}' was removed`;
+        return generateResult('removed', resultPath);
       }
       if (item.status === 'changed') {
         if (isItemObject(item.oldValue) && isItemObject(item.newValue)) {
-          return `Property '${generatedPath.join('.')}' was updated. From [complex value] to [complex value]`;
+          return generateResult('changed', resultPath, '[complex value]', '[complex value]');
         }
         if (isItemObject(item.oldValue)) {
-          return `Property '${generatedPath.join('.')}' was updated. From [complex value] to '${item.newValue}'`;
+          if (!isStringValue(item.newValue)) return generateResult('changed', resultPath, '[complex value]', `${item.newValue}`);
+          return generateResult('changed', resultPath, '[complex value]', `'${item.newValue}'`);
         }
         if (isItemObject(item.newValue)) {
-          return `Property '${generatedPath.join('.')}' was updated. From '${item.oldValue}' to [complex value]`;
+          if (!isStringValue(item.oldValue)) return generateResult('changed', resultPath, `${item.oldValue}`, '[complex value]');
+          return `Property '${resultPath}' was updated. From '${item.oldValue}' to [complex value]`;
         }
         if (!isStringValue(item.newValue) && !isStringValue(item.oldValue)) {
-          return `Property '${generatedPath.join('.')}' was updated. From ${item.oldValue} to ${item.newValue}`;
+          return generateResult('changed', resultPath, `${item.oldValue}`, `${item.newValue}`);
         }
         if (!isStringValue(item.oldValue)) {
-          return `Property '${generatedPath.join('.')}' was updated. From ${item.oldValue} to '${item.newValue}'`;
+          return generateResult('changed', resultPath, `${item.oldValue}`, `'${item.newValue}'`);
         }
         if (!isStringValue(item.newValue)) {
-          return `Property '${generatedPath.join('.')}' was updated. From '${item.oldValue}' to ${item.newValue}`;
+          return generateResult('changed', resultPath, `'${item.oldValue}'`, `${item.newValue}`);
         }
-        return `Property '${generatedPath.join('.')}' was updated. From '${item.oldValue}' to '${item.newValue}'`;
+        return generateResult('changed', resultPath, `'${item.oldValue}'`, `'${item.newValue}'`);
       }
       if (item.status === 'bothObjects') return wrapper(item.children, generatedPath);
       return null;
