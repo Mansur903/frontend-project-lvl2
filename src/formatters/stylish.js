9fd9c9ba29d3c5ef60f2bf36256d17b98ac60cd8
Mansur903 <mansur98@yandex.ru>
2021-04-19 16:09:21 +0300

    1.292

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,11 +1,11 @@
 const makeStringFromObject = (value, replacer = ' ', spacesCount = 1, curDepth) => {
-  const defaultIndent = 1;
+  const defaultIndent = 4;
   const iter = (currentValue, depth) => {
     if (typeof currentValue !== 'object' || currentValue === null) {
       if (currentValue === null || currentValue === undefined) return 'null';
       return currentValue.toString();
     }
-    const indentSize = depth * spacesCount;
+    const indentSize = depth * 4 * spacesCount;
     const currentIndent = replacer.repeat(indentSize + defaultIndent);
     const bracketIndent = replacer.repeat(indentSize);
     const lines = Object
@@ -19,26 +19,29 @@ const makeStringFromObject = (value, replacer = ' ', spacesCount = 1, curDepth) 
   };
   return iter(value, curDepth);
 };
-const separator = '  ';
-
+const separator = ' ';
+const spacesCount = 1;
 export default function stylish(data) {
   function wrapper(thisData, depth) {
     const result = thisData.map((item) => {
       if (item.status === 'bothObjects') {
         const newDepth = depth + 1;
-        return (`${separator.repeat(depth)}${item.name}: {\n${wrapper(item.children, newDepth)}\n${separator.repeat(depth)}}`);
+        if (depth === 1) {
+          return (`${separator.repeat(depth + 3)}${item.name}: {\n${wrapper(item.children, newDepth)}\n${separator.repeat(depth * 4)}}`);
+        }
+        return (`${separator.repeat(depth * 4 - 2)}  ${item.name}: {\n${wrapper(item.children, newDepth)}\n${separator.repeat(depth * 4)}}`);
       }
       if (item.status === 'added') {
-        return (`${separator.repeat(depth - 1)}+ ${item.name}: ${makeStringFromObject(item.value, separator, 1, depth)}`);
+        return (`${separator.repeat(depth * 4 - 2)}+ ${item.name}: ${makeStringFromObject(item.value, separator, spacesCount, depth)}`);
       }
       if (item.status === 'removed') {
-        return (`${separator.repeat(depth - 1)}- ${item.name}: ${makeStringFromObject(item.value, separator, 1, depth)}`);
+        return (`${separator.repeat(depth * 4 - 2)}- ${item.name}: ${makeStringFromObject(item.value, separator, spacesCount, depth)}`);
       }
       if (item.status === 'changed') {
-        return (`${separator.repeat(depth - 1)}- ${item.name}: ${makeStringFromObject(item.oldValue, separator, 1, depth)}\n${separator.repeat(depth - 1)}+ ${item.name}: ${makeStringFromObject(item.newValue, separator, 1, depth)}`);
+        return (`${separator.repeat(depth * 4 - 2)}- ${item.name}: ${makeStringFromObject(item.oldValue, separator, spacesCount, depth)}\n${separator.repeat(depth * 4 - 2)}+ ${item.name}: ${makeStringFromObject(item.newValue, separator, spacesCount, depth)}`);
       }
       if (item.status === 'unchanged') {
-        return (`${separator.repeat(depth - 1)}  ${item.name}: ${makeStringFromObject(item.value, separator, 1, depth)}`);
+        return (`${separator.repeat(depth * 4 - 2)}  ${item.name}: ${makeStringFromObject(item.value, separator, spacesCount, depth)}`);
       }
       return null;
     }).join('\n');
